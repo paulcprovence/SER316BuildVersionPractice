8db8c716f71bb89243779c994f8b919fef8f122e
paulcprovence <pproven1@asu.edu>
2023-04-19 21:13:03 -0500

    update build to create a class to add version

---
+++ src/main/java/BuildConfig.java
@@ -0,0 +1,3 @@
+public class BuildConfig {
+    public static final String APP_VERSION = "1.0.1";
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,10 @@ public class Main{
 
         System.out.println("Hello, world!");
 
+        
+        System.out.println(BuildConfig.APP_VERSION);
+        
+
     }
 
 
